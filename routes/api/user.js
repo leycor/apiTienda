aaacb9fa6ef4ff62b2b85b2ebc8e8d1c4ef1d6b0
leycor <60824907+leycor@users.noreply.github.com>
2022-01-22 01:36:47 -0400

    Generar token al coincidir los datos

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,8 +1,16 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs')
+const jwt = require('jwt-simple')
+const moment = require('moment')
 
 // Modelo
 const userModel = require('../../db/models/userModel');
+
+// http://localhost:3000/api/users
+router.get('/', async(req, res)=> {
+    const result = await userModel.getAllUsers()
+    res.json(result)
+})
 
 
 router.post('/register', async(req, res) =>{
@@ -31,24 +39,32 @@ router.post('/login', async(req, res) => {
     const email = req.body.email
     const result = await userModel.getEmail(email)
 
-    if(!result) res.json('Datos incorrectos')
+    // Responder con un error si el email no existe
+    if(!result) res.json({error: true, data: 'Password o email incorrectos'})
 
     try {
         const passOkey = await bcrypt.compare(password.toString(), result.password)
-        if(!passOkey) res.json('Datos incorrectos')
-        res.json(result)
+
+        // Comparar si la password coincide con el hash de la base de datos
+        if(!passOkey) res.json({error: true, data: 'Password o email incorrecto'})
+
+        // Si las password coinciden enviar token
+        res.json({error: false, data: createToken(result)})
     } catch (error) {
         console.log(error)
     }
 
 })
 
+const createToken = (user) => {
+    const payload = {
+        userId: user.id,
+        createdAt: moment().unix(),
+        expiredAt: moment().add(5,'minutes').unix()
+    }
 
-// http://localhost:3000/api/users
-router.get('/', async(req, res)=> {
-    const result = await userModel.getAllUsers()
-    res.json(result)
-})
+    return jwt.encode(payload, 'secred words')
+}
 
 
 module.exports = router;
